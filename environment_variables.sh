afa635c2d3f1650255b423dce4f487747973f4a6
John Grando <jgrando@gard.com>
2021-02-01 08:34:49 -0500

    made environment variable declarations public

---
+++ environment_variables.sh
@@ -0,0 +1,5 @@
+# non-secret environment variables to configure access
+export ENERGYPLUS_ROOT_DIR=~/.wine/drive_c/EnergyPlusV9-4-0
+export ENERGYPLUS_EXPANDOBJECTS_ROOT_DIR=~/Documents/Gard/pyExpandObjects
+export PYTHONPATH="$ENERGYPLUS_EXPANDOBJECTS_ROOT_DIR:$PYTHONPATH"
+export COVERAGE_RCFILE="$ENERGYPLUS_EXPANDOBJECTS_ROOT_DIR/test/.coveragerc"
