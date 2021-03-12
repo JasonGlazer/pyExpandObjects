39cb4e8f2bcf4b359eeb592801fea18e45021aa0
John Grando <jgrando@gard.com>
2021-03-11 19:25:29 -0500

    cleaned up test output

--- test/test_hvac_template.py
+++ test/test_hvac_template.py
@@ -103,7 +103,7 @@ class TestHVACTemplateObject(unittest.TestCase):
         self.assertEqual(len(self.hvac_template.templates.keys()), 1)
         return
 
-    @_test_logger()
+
     def test_n_hvac_objects_one_template_returns_true(self):
         self.hvac_template.load_epjson({
             **minimum_objects_d,
