28c73d5df4e64a865f90011a711da245b674fb42
John Grando <jgrando@gard.com>
2021-06-24 21:07:22 -0400

    fixed base file issues

---
+++ tests/simulations/plant/test_plant_boiler.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from tests.simulations import BaseSimulationTest
+from src.epjson_handler import EPJSON
+
+test_dir = Path(__file__).parent.parent.parent
+
+
+class TestSimulationsPlantBoiler(BaseSimulationTest):
+    def setUp(self):
+        self.ej = EPJSON()
+        base_idf_file_path = test_dir.joinpath('..', 'simulation', 'ExampleFiles', 'HVACTemplate-5ZoneVAVWaterCooled.idf')
+        base_copy_file_path = self._copy_to_test_directory(base_idf_file_path)
+        # read in base file, then edit inputs for alternate tests
+        self.base_epjson = self.get_epjson_object_from_idf_file(base_copy_file_path)
+        return
+
+    def teardown(self):
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:Plant:Boiler:boiler_type_hot_water_boiler")
+    def test_boiler_type_hot_water_boiler(self):
+        # self.base_epjson['HVACTemplate:Plant:Boiler']['Main Boiler']['boiler_type'] = 'HotWaterBoiler'
+        base_file_path = self.create_idf_file_from_epjson(epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertIsNotNone(epjson_output.get('Boiler:HotWater'))
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:Plant:Boiler:boiler_type_hot_water_boiler")
+    def test_boiler_type_district_hot_water(self):
+        self.base_epjson['HVACTemplate:Plant:Boiler']['Main Boiler']['boiler_type'] = 'DistrictWaterBoiler'
+        base_file_path = self.create_idf_file_from_epjson(epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        return
