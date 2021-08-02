b8d1b715a2d3ec06761974447c2afa0b5efb8c30
John Grando <jgrando@gard.com>
2021-08-01 23:54:12 -0400

    VAV system option testing started

--- tests/simulations/system/test_system_vav.py
+++ tests/simulations/system/test_system_vav.py
@@ -27,5 +27,110 @@ class TestSimulationsSystemVAV(BaseSimulationTest):
         epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
         self.assertEqual(
             'OCCUPY-1',
-            epjson_output['Fan:VariableVolume']['DXVAV Sys 1 Supply Fan']['availability_schedule_name'])
+            epjson_output['Fan:VariableVolume']['VAV Sys 1 Supply Fan']['availability_schedule_name'])
         return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:supply_fan_maximum_flow_rate")
+    def test_supply_fan_maximum_flow_rate(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1']['supply_fan_maximum_flow_rate'] = 2
+        base_file_path = self.create_idf_file_from_epjson(epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertEqual(
+            2.0,
+            epjson_output['Sizing:System']['VAV Sys 1 Sizing System']['cooling_supply_air_flow_rate'])
+        self.assertEqual(
+            'Flow/System',
+            epjson_output['Sizing:System']['VAV Sys 1 Sizing System']['cooling_supply_air_flow_rate_method'])
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:supply_fan_minimum_flow_rate")
+    def test_supply_fan_minimum_flow_rate(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1']['supply_fan_minimum_flow_rate'] = 0.25
+        base_file_path = self.create_idf_file_from_epjson(epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertEqual(
+            0.25,
+            epjson_output['Fan:VariableVolume']['VAV Sys 1 Supply Fan']['fan_power_minimum_air_flow_rate'])
+        self.assertEqual(
+            'FixedFlowRate',
+            epjson_output['Fan:VariableVolume']['VAV Sys 1 Supply Fan']['fan_power_minimum_flow_rate_input_method'])
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:supply_fan_total_efficiency")
+    def test_supply_fan_total_efficiency(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1']['supply_fan_total_efficiency'] = 0.75
+        base_file_path = self.create_idf_file_from_epjson(epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertEqual(
+            0.75,
+            epjson_output['Fan:VariableVolume']['VAV Sys 1 Supply Fan']['fan_total_efficiency'])
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:supply_fan_delta_pressure")
+    def test_supply_fan_delta_pressure(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1']['supply_fan_delta_pressure'] = 1100
+        base_file_path = self.create_idf_file_from_epjson(epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertEqual(
+            1100,
+            epjson_output['Fan:VariableVolume']['VAV Sys 1 Supply Fan']['pressure_rise'])
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:supply_fan_motor_efficiency")
+    def test_supply_fan_motor_efficiency(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1']['supply_fan_motor_efficiency'] = 0.8
+        base_file_path = self.create_idf_file_from_epjson(epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath('..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertEqual(
+            0.8,
+            epjson_output['Fan:VariableVolume']['VAV Sys 1 Supply Fan']['motor_efficiency'])
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:"
+                                              "supply_fan_motor_in_air_stream_fraction")
+    def test_supply_fan_motor_in_air_stream_fraction(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1'][
+            'supply_fan_motor_in_air_stream_fraction'] = 0.8
+        base_file_path = self.create_idf_file_from_epjson(
+            epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath(
+            '..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertEqual(
+            0.8,
+            epjson_output['Fan:VariableVolume']['VAV Sys 1 Supply Fan']['motor_in_airstream_fraction'])
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:"
+                                              "cooling_coil_type_chilled_water")
+    def test_cooling_coil_type_chilled_water(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1'][
+            'cooling_coil_type'] = 'ChilledWater'
+        base_file_path = self.create_idf_file_from_epjson(
+            epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath(
+            '..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertIsNotNone(
+            epjson_output['Coil:Cooling:Water'].get('VAV Sys 1 Cooling Coil'))
+        return
+
+    @BaseSimulationTest._test_logger(doc_text="Simulation:System:VAV:"
+                                              "cooling_coil_type_chilled_water_detailed_flat_model")
+    def test_cooling_coil_type_chilled_water_detailed_flat_model(self):
+        self.base_epjson['HVACTemplate:System:VAV']['VAV Sys 1'][
+            'cooling_coil_type'] = 'ChilledWaterDetailedFlatModel'
+        base_file_path = self.create_idf_file_from_epjson(
+            epjson=self.base_epjson, file_name='base_pre_input.epJSON')
+        self.perform_full_comparison(base_idf_file_path=base_file_path)
+        epjson_output = self.ej._get_json_file(test_dir.joinpath(
+            '..', 'simulation', 'test', 'test_input_epjson.epJSON'))
+        self.assertIsNotNone(
+            epjson_output['Coil:Cooling:Water:DetailedGeometry'].get('VAV Sys 1 Cooling Coil'))
+        return
+
